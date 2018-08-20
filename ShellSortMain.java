6c1df46a43c37fa99c028bdf46a8aa12b5759688
MohammedUves <42522589+MohammedUves@users.noreply.github.com>
2018-08-20 16:39:32 +0530

    Add files via upload

---
+++ ShellSortMain.java
@@ -0,0 +1,42 @@
+package sortingAlgorithms.shellSort;
+import java.util.Arrays;
+
+public class ShellSortMain {
+
+	public static void main(String[] args) {
+		int[] array={22,53,33,12,74,65,887,125,37,977};
+		System.out.println("Before Sorting: ");
+		System.out.println(Arrays.toString(array));
+		System.out.println("========================");
+		System.out.println("After Sorting: ");
+		array=shellSort(array);
+		System.out.println(Arrays.toString(array));
+	}
+	
+	private static int[] shellSort(int[] array){
+		//First part uses the Knuth's interval sequence
+		int h=1;
+		while(h<=array.length/3)
+		{
+			h=3*h+1; //h is equal to highest sequence of h<=length/3
+			//(1,4,13,40....)
+		}
+		
+		//next part
+		while(h>0) { //For array of length 10,h=4
+			//This step  is similar to insertion sort below
+			for(int i=0;i<array.length;i++){
+				int temp=array[i];
+				int j;
+				
+				for(j=i;j>h-1&&array[j-h]>=temp;j=j-h){
+					array[j]=array[j-h];
+				}
+					array[j]=temp;
+			}
+			h=(h-1)/3;
+			}
+		return array;
+	}
+
+}
